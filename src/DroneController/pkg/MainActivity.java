a1a9166f4edfaab870f5a474393af00d2c1c3236
sdeltombe <sdeltombe@gmail.com>
2014-03-12 17:59:04 +0100

    Version stable avec déplacement du drone

---
+++ src/DroneController/pkg/MainActivity.java
@@ -0,0 +1,99 @@
+package DroneController.pkg;
+
+import dronecontrollerlib.pkg.DCManager;
+import DroneController.pkg.R;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import dronecontrollerlibandroid.pkg.*;
+
+
+
+public class MainActivity extends Activity implements CustomEventActivity
+{
+   
+    private SPListener myListener; 
+    private AndroidUtility utility;
+    int nb=0;
+     
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.main);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        utility = new AndroidUtility();
+        utility.SetView(this);
+        AndroidFactory factory = new AndroidFactory(utility);
+        Object[] tab = new Object[1];
+        tab[0] = (CustomEventActivity)this;
+        //creation du manager en passant en arg la factory et la classe d'utilité
+        //DCManager manager = new DCManager(factory,StandardFactory.WIIMOTE_TYPE,tab);
+        DCManager manager = new DCManager(factory,AndroidFactory.SMARTPHONE_TYPE,tab);
+        //Lancement du traitement d'ecoute du controller
+        manager.run();
+    }
+    
+    public void SetListener(SPListener listener)
+    {
+        this.myListener = listener;
+    }
+    
+    public void TakeOff(View view)
+    {
+       if(myListener!=null)
+            myListener.TakeOff();
+    }
+     public void Landing(View view)
+    {
+        if(myListener!=null)
+            myListener.Landing();
+    }
+    public void refresh()
+    {
+        utility.printToastHisto();
+    }
+    
+  
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+	return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+         case R.id.action_refresh:
+            refresh();
+            return true;
+          case R.id.action_settings:
+            //openSettings();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+
+    }
+    
+     @Override
+    protected void onDestroy()
+    {
+      utility.EndToast();
+    }
+    
+    
+    
+    
+}
